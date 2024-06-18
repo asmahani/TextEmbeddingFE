a5a3ac2bd2f06a775a841ec540d4da269fdc75c4
Alireza Mahani <alireza.s.mahani@gmail.com>
2024-06-18 07:08:33 -0400

    i was able to set up the google cloud environment faster than i expected :) successfully tested a hello world example for text generation

--- explore/explore_google_ai.py
+++ explore/explore_google_ai.py
@@ -1,11 +1,15 @@
-PROJECT_ID = PROJECT_ID
-REGION = us-central1
-MODEL_ID = MODEL_ID
-
 import vertexai
-from vertexai.language_models import TextEmbeddingModel
+from vertexai.generative_models import GenerativeModel
 
-vertexai.init(project=PROJECT_ID, location=REGION)
+# TODO(developer): Update and un-comment below line
+project_id = "moonlit-helper-426810-a2"
 
-model = TextEmbeddingModel.from_pretrained(MODEL_ID)
-embeddings = model.get_embeddings(...)
+vertexai.init(project=project_id, location="us-central1")
+
+model = GenerativeModel(model_name="gemini-1.5-flash-001")
+
+response = model.generate_content(
+    "What's a good name for a flower shop that specializes in selling bouquets of dried flowers?"
+)
+
+print(response.text)
