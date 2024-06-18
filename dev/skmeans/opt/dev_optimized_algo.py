9a571098173864b226f8f63167296f7864eb0f54
Alireza Mahani <alireza.s.mahani@gmail.com>
2024-06-17 21:23:10 -0400

    arrived at a decent python implementation of lloyd single update. despite being all in python, it is now much faster, expected to need about 1sec for 10 starts for 3072 features and 10 clusters

--- dev/skmeans/opt/dev_optimized_algo.py
+++ dev/skmeans/opt/dev_optimized_algo.py
@@ -3,9 +3,29 @@ import numpy as np
 def skmeans_lloyd_update(
         X
         , centroids
+        , similarities
 ):
     n_clusters, n_features = centroids.shape
-    return n_clusters, n_features
+    n_obs = X.shape[0]
+
+    new_labels = np.empty(n_obs, dtype = int)
+    new_sizes = np.zeros(n_clusters, dtype = int)
+    new_centroids = np.zeros_like(centroids)
+    for n in range(n_obs):
+        new_labels[n] = np.argmax(similarities[n, :])
+        new_sizes[new_labels[n]] += 1
+        new_centroids[new_labels[n], :] += X[n, :]
+    
+    if np.any(new_sizes == 0):
+        raise RuntimeError('One or more clusters are empty')
+
+    new_centroids = new_centroids / np.linalg.norm(new_centroids, axis = 1).reshape(-1, 1)
+    new_similarities = np.dot(X, new_centroids.T)
+    new_inertia = np.sum(1 - np.max(new_similarities, axis = 1))
+
+    frobenius_norm = np.sqrt(np.sum((new_centroids - centroids) ** 2))
+
+    return new_similarities, new_labels, new_sizes, new_centroids, new_inertia, frobenius_norm
 
 ####
 
@@ -16,8 +36,32 @@ dfEmbeddings = pd.read_csv(
 )
 
 my_n_cluster = 10
-my_n_features = 5
-X = dfEmbeddings.iloc[:, 2:(2 + my_n_features)].to_numpy()
-init_centroids = X[np.random.choice(X.shape[0], size = my_n_cluster, replace = False), :]
+my_n_features = 3072
 
-skmeans_lloyd_update(X, init_centroids)
+X = dfEmbeddings.iloc[:, 2:(2 + my_n_features)].to_numpy()
+X = np.apply_along_axis(lambda x: x / np.sqrt(np.sum(x * x)), 1, X)
+X_unique = np.unique(X, axis = 0)
+
+init_indices = np.random.choice(X_unique.shape[0], size = my_n_cluster, replace = False)
+init_centroids = X_unique[init_indices, :]
+init_similarities = np.dot(X, init_centroids.T)
+
+my_similarities = init_similarities
+my_centroids = init_centroids
+
+import time
+
+t = time.time()
+for n in range(50):
+    my_similarities, my_labels, my_sizes, my_centroids, my_inertia, my_delta = skmeans_lloyd_update(
+        X, my_centroids
+        , my_similarities
+    )
+    print(f'iter {n+1}, intertia: {my_inertia}, fb norm: {round(my_delta, 2)}')
+    if my_delta < 1e-4:
+        break
+t = time.time() - t
+print(f'time: {round(t, 2)}sec')
+
+
+
